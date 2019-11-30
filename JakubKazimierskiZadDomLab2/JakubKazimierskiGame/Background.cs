3f101692751f5c8e3207c1864d3405d86fbcf972
JakubKazimierski <j.m.kazimierski@gmail.com>
2019-11-30 20:30:48 +0100

    Frist abstract class, class of background

---
+++ JakubKazimierskiZadDomLab2/JakubKazimierskiGame/Background.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakubKazimierskiGame
+{
+  abstract class Background
+    {
+        protected string colorBackground;
+        protected int obstaclesBackground;
+
+        public virtual string GetColorBackground()
+        {
+            return colorBackground;
+        }
+
+        public virtual int GetObstaclesBackground()
+        {
+            return obstaclesBackground;
+        }
+
+        public virtual void SetColorBackground(string colorName)
+        {
+            colorBackground = colorName;
+        }
+
+        public virtual void SetObstaclesBackground(int amount)
+        {
+            obstaclesBackground = amount;
+        }
+    
+    }
+}
